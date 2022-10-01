d48119595fba833d6a74c2b44dc4933e1f04aa84
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-01 05:37:44 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Zaid Neurothrone on 2022-10-01.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
